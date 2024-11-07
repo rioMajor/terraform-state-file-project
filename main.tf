97977d981daa2aa84a48046b05b6a2b75eef2a05
Sharan N <122720722+rioMajor@users.noreply.github.com>
2024-11-07 13:36:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,25 @@
+provider "aws" {
+    region = "ap-south-1"
+  
+}
+resource "aws_instance" "sharan" {
+    instance_type = "t2.micro"
+    ami = "ami-0dee22c13ea7a9a67"
+  
+}
 
+resource "aws_s3_bucket" "sharan-demo" {
+    bucket = "sharan-s3-demo-xyz"
+
+  
+}
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
